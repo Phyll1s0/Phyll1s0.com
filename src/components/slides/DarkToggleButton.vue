9c40f92cfc480814e20fa4e079a9b3a72cf0e884
Phyll1s0 <louhao088@sjtu.edu.cn>
2026-04-29 19:18:56 +0800

    fix: 修复暗黑模式切换按钮事件传递

--- src/components/slides/DarkToggleButton.vue
+++ src/components/slides/DarkToggleButton.vue
@@ -5,7 +5,7 @@ import { isDark, toggleDark } from '~/logics'
 <template>
   <button
     class="bg-[#42b883] rounded border-b-2 border-green-900 text-white text-sm px-2 pt-1.5 pb-1 inline-block !outline-none hover:bg-opacity-85"
-    @click="toggleDark"
+    @click="toggleDark($event)"
   >
     <div class="flex">
       <div i-ri-sun-line dark:i-ri-moon-line />
